fdc3dc8b0b5cd45488f8ed3ddc93b14bd04c7af2
Anasurim <108210477+Anasurim@users.noreply.github.com>
2022-12-15 17:14:42 +0000

    Deploying to gh-pages from @ Anasurim/icecream-team2@23ff106b03783bcbf937df4829c80f33afcf9a98 🚀

---
+++ index.2042de83.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".btn--to-top");document.addEventListener("scroll",(()=>{(document.documentElement||document.body).scrollTop>100?e.classList.remove("hidden"):e.classList.add("hidden")}));e.addEventListener("click",(()=>{document.body.scrollIntoView({behavior:"smooth"})}));
+//# sourceMappingURL=index.2042de83.js.map
---
+++ index.d528d674.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".btn--to-top");document.addEventListener("scroll",(function(){(document.documentElement||document.body).scrollTop>100?e.classList.remove("hidden"):e.classList.add("hidden")}));e.addEventListener("click",(function(){document.body.scrollIntoView({behavior:"smooth"})}))}();
+//# sourceMappingURL=index.d528d674.js.map
