613075151d7b5c8916436cb875c64ccae7770208
Lebedynskyi Roman <cliffcrystall@gmail.com>
2022-12-15 17:37:13 +0100

    js to modal added but it doesnot work

--- src/js/scroll-to-top.js
+++ src/js/scroll-to-top.js
@@ -1,13 +1,13 @@
 // Get the button:
 let mybutton = document.getElementById('myBtn');
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 30px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     mybutton.style.display = 'flex';
   } else {
     mybutton.style.display = 'none';
