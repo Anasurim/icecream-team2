d22197137e8df0274ffedc326bc94c41a4174e18
Anasurim <108210477+Anasurim@users.noreply.github.com>
2022-12-18 01:44:41 +0000

    Deploying to gh-pages from @ Anasurim/icecream-team2@a4c8c2cfb3772a4621ffddbd78c953229fe6364a 🚀

---
+++ index.1942294e.js
@@ -0,0 +1,2 @@
+window.onscroll=()=>function(){const e=document.querySelector("[data-header]"),n=e.offsetHeight;window.pageYOffset>n?e.classList.add("no-transparency"):e.classList.remove("no-transparency")}();
+//# sourceMappingURL=index.1942294e.js.map
---
+++ index.8aaf5ff8.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){return e=document.querySelector("[data-header]"),n=e.offsetHeight,void(window.pageYOffset>n?e.classList.add("no-transparency"):e.classList.remove("no-transparency"));var e,n};
+//# sourceMappingURL=index.8aaf5ff8.js.map
