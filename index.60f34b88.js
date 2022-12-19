7fe1322e33580823e6c145660317b4c2e72c92c3
Anasurim <108210477+Anasurim@users.noreply.github.com>
2022-12-19 12:25:13 +0000

    Deploying to gh-pages from @ Anasurim/icecream-team2@d9df9f6e4b24ee3c71709163d17eb115c850a99a 🚀

---
+++ index.60f34b88.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{slidesToShow:1,autoplay:!0,autoplaySpeed:3e3,direction:"horizontal",loop:!0,speed:3e3,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.60f34b88.js.map
