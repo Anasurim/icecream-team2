d9df9f6e4b24ee3c71709163d17eb115c850a99a
Lebedynskyi Roman <cliffcrystall@gmail.com>
2022-12-19 13:24:29 +0100

    added swiper to gallery sevtion

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,20 +1,20 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
   direction: 'horizontal',
   loop: true,
-
+  speed: 3000,
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    clickable: true,
   },
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
